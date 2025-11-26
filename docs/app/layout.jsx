68374e9e78c8ae61553d3d003d4447840e013466
Oliver Kwun-Morfitt <oliverkwunmorfitt@gmail.com>
2025-11-26 15:03:46 -0500

    added bg

--- docs/app/layout.jsx
+++ docs/app/layout.jsx
@@ -1,39 +1,14 @@
-import { Footer, Layout, Navbar } from 'nextra-theme-docs'
-import { Head } from 'nextra/components'
-import { getPageMap } from 'nextra/page-map'
-import 'nextra-theme-docs/style.css'
+import './globals.css'
 
 export const metadata = {
-  title: 'LLM Structured Output Engine',
-  description: 'Documentation for LLM Structured Output Engine'
+  title: 'parsec',
+  description: 'Structured output generation for LLMs'
 }
 
-const navbar = (
-  <Navbar
-    logo={<b>LLM Structured Output Engine</b>}
-    projectLink="https://github.com/olliekm/llm-structured-output-engine"
-  />
-)
-const footer = <Footer>MIT {new Date().getFullYear()} © LLM Structured Output Engine</Footer>
-
-export default async function RootLayout({ children }) {
+export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      dir="ltr"
-      suppressHydrationWarning
-    >
-      <Head />
-      <body>
-        <Layout
-          navbar={navbar}
-          pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/oliverkwun-morfitt/llm-structured-output-engine/tree/main/docs"
-          footer={footer}
-        >
-          {children}
-        </Layout>
-      </body>
+    <html lang="en" suppressHydrationWarning>
+      <body>{children}</body>
     </html>
   )
 }
---
+++ docs/app/page.jsx
@@ -0,0 +1,51 @@
+import Link from 'next/link'
+import LiquidEther from '@/components/LiquidEther';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 text-center bg-black text-white">
+       
+      <h1 className="text-3xl font-mono font-bold mb-4">
+        parsec
+      </h1>
+      <p className="text-2xl mb-8 text-gray-150">
+        Structured output generation for LLMs
+      </p>
+      <Link
+        href="/docs"
+        className="px-4 py-2 text-lg bg-white text-black rounded-full hover:bg-gray-200 transition-colors cursor-pointer"
+      >
+        View Documentation
+      </Link>
+       {/* <LiquidEther
+        colors={[ '#5227FF', '#FF9FFC', '#B19EEF' ]}
+        mouseForce={20}
+        cursorSize={100}
+        isViscous={false}
+
+        viscous={30}
+
+        iterationsViscous={32}
+
+        iterationsPoisson={32}
+
+        resolution={0.5}
+
+        isBounce={false}
+
+        autoDemo={true}
+
+        autoSpeed={0.5}
+
+        autoIntensity={2.2}
+
+        takeoverDuration={0.25}
+
+        autoResumeDelay={3000}
+
+        autoRampDuration={0.6}
+
+  /> */}
+    </div>
+  )
+}
