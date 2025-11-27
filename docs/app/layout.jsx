0993aeea4391a7b11ea7649c43e403648a092406
Oliver Kwun-Morfitt <oliverkwunmorfitt@gmail.com>
2025-11-27 13:01:46 -0500

    added favicon

--- docs/app/layout.jsx
+++ docs/app/layout.jsx
@@ -2,7 +2,10 @@ import './globals.css'
 
 export const metadata = {
   title: 'parsec',
-  description: 'Structured output generation for LLMs'
+  description: 'Structured output generation for LLMs',
+  icons: {
+    icon: '/favicon.ico',
+  }
 }
 
 export default function RootLayout({ children }) {
