485d9d09783605e0ca25207c80fd0a4f348309d0
Oliver Kwun-Morfitt <oliverkwunmorfitt@gmail.com>
2025-12-04 20:08:25 -0500

    updated docs and fixed yaml dependency

--- docs/app/docs/_meta.js
+++ docs/app/docs/_meta.js
@@ -6,6 +6,8 @@ export default {
   'model-adapters': 'Model Adapters',
   'engines': 'Engines',
   'validators': 'Validators',
+  'prompt-templates': 'Prompt Templates',
+  'caching': 'Caching',
   'dataset-collection': 'Dataset Collection',
   'testing': 'Testing',
   'logging': 'Logging'
